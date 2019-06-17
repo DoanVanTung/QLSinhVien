9565e8f8c29e7b10abc4814546add4555b6a289f
nguyenkhaik58utc <nguyenkhaik58utc@gmail.com>
2019-06-17 09:37:15 +0700

    create add_sinhvien.java

---
+++ QLSinhVien/src/main/java/Controller/Delete_sv.java
@@ -0,0 +1,57 @@
+package Controller;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class Delete_sv {
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+
+	private static Connection getConnection() throws SQLException, ClassNotFoundException {
+		Connection con = ConnectionDB.getInstance().getConnection();
+		return con;
+	}
+
+	@RequestMapping(value = "/Delete/{aaa}", method = RequestMethod.POST, headers = "Accept=*/*", produces = {
+			MediaType.APPLICATION_JSON_VALUE, //
+			MediaType.APPLICATION_XML_VALUE })
+	@ResponseBody
+	public void deleteStudent(@PathVariable("aaa") String empNo) throws ClassNotFoundException, SQLException {
+
+		
+		String sql="delete from sinhvien where maSV='" + empNo + "'";
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
+// nếu method POST thì return phải ntn ?
+//contentType : la gì? tai sao phải dùng? dùng để làm gì?
+//dataType  la gì? tai sao phải dùng? dùng để làm gì?
+
