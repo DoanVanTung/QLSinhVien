ee50e8d52628f557ad8efb9b2aec225bade85819
khonghai <khonghai20199@gmail.com>
2019-06-19 16:41:46 +0700

    test

--- QLSinhVien/src/main/java/Controller/CertificateController.java
+++ QLSinhVien/src/main/java/Controller/CertificateController.java
@@ -37,6 +37,7 @@ public class CertificateController {
 	private Connection conn;
 	private PreparedStatement pstmt;
 	private ResultSet rs;
+//aaaaaa
 //bbbbb
 	/**
 	 * phuong thuc nay duoc su dung de tra ve connect voi database
